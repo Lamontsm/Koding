8d5948bbe2da3eb421645d7399d5431c66c88543
Lamontsm <steven@lamonaco.com>
2014-12-23 01:34:04 +0000

    played around with some web forms of python

---
+++ Web/Steve_1.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+import os
+import platform
+print "Content-Type: text/html"
+print
+print """\
+<!DOCTYPE html>
+<html lang="en">
+<head>  <meta charset="utf-8">
+  <title>Steve's Hello World Trial</title>
+  <title>Featuring Python, Flask, and XML<title>
+  <meta name="viewport" content="width=device-width,initial-scale=1">
+  <link rel="stylesheet" href="//koding.com/hello/css/style.css">
+  <!--[if IE]>
+      	<script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+  <link href='//fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+</head>
+<body class="python">
+  
+
+import os
+print "Content-type: text/html"
+/Workspaces/HelloWorld2/FlaskHello2.py
+</body>
+</html>
+"""
